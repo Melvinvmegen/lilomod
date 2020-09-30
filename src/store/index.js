54a3fc8b55f52388e0cff81ea790d4889f47c96d
Melvin van Megen <melvin.vmegen@gmail.com>
2020-09-30 12:48:55 +0200

    auth store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,50 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from 'axios'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    token: null,
+    user: null
+  },
+  getters: {
+    user (state) {
+      return state.user
+    }
+  },
+  mutations: {
+    authToken (state, userData) {
+      state.token = userData.token
+    },
+    storeUser (state, user) {
+      state.user = user
+    }
+  },
+  actions: {
+    login ({commit, dispatch}, authData) {
+      console.log(authData)
+      axios.post('api/users/sign_in', {
+        user: {
+          email: authData.email,
+          password: authData.password
+        }
+      })
+        .then(res => {
+          commit('authToken', {
+            token: res.data.token
+          })
+          dispatch('fetchUser')
+        })
+        .catch(error => console.log(error))
+    },
+    fetchUser ({commit}) {
+      axios.get('api/users/current')
+        .then(res => {
+          commit('storeUser', res.data.user)
+        })
+    }
+  },
   modules: {}
 });
