c850d989e447ac4121c404c7d49cdff51ab49574
Melvin van Megen <melvin.vmegen@gmail.com>
2020-10-11 13:04:48 +0200

    services

---
+++ src/views/Services.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  .d-flex
+    v-container#serices
+      h1.text-center Je suis les services
+      br
+      h2.h4.font-italic.text-center Informations de qualité pour votre santé, prévention et bien-être
+      br
+      h3.text-center Découvrez mes prestations :
+      .row.justify-center
+        .col-lg-10
+
+</template>
+
+<script>
+
+export default {
+  name: 'Services',
+  components: {
+  },
+  computed: {
+    prestations () {
+      return this.$store.state.prestations.prestations
+    }
+  },
+  created: function() {
+    if (this.$store.state.prestations.prestations.length > 0) {
+      return
+    }
+    this.$store.dispatch('getPrestations')
+  }
+}
+</script>
+
+<style>
+</style>
