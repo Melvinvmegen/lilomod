45106a2f12aae0f1bcc553a8551be045e95c77ac
Melvin van Megen <melvin.vmegen@gmail.com>
2020-10-11 13:03:57 +0200

    homepage

---
+++ src/views/Homepage.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .d-flex
+    v-container
+      h1 Je suis la homepage !
+
+</template>
+
+<script>
+export default {
+  name: 'Homepage'
+}
+</script>
+
+<style>
+
+</style>
