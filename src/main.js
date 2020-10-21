82de30cbed6029c135f107f4235c20923ad9937c
Melvin van Megen <melvin.vmegen@gmail.com>
2020-10-21 16:38:31 +0200

    fix linter error

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import Vuelidate from "vuelidate";
 
 Vue.use(Vuelidate);
 Vue.use(VueAxios);
-axios.defaults.baseURL = process.env.VUE_APP_API_URL || "https://lilomod.herokuapp.com/";
+axios.defaults.baseURL = process.env.VUE_APP_API_URL ||"https://lilomod.herokuapp.com/";
 axios.defaults.headers.get["Accepts"] = "application/json";
 
 Vue.config.productionTip = false;
